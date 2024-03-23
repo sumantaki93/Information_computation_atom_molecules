b8e7d221b9315c3edd5989d56a0f1d550a21ab0c
SUMAN HAZRA <97603082+sumantaki93@users.noreply.github.com>
2024-03-23 22:14:18 +0000

    Add files via upload

--- Source_code/basisfn.f90
+++ Source_code/basisfn.f90
@@ -43,31 +43,22 @@
                      r_r
           
            p=(l+m+n)
-          ! write(*,*) p 
            np=(4*alpha)**(p)
-            !write(*,*) np
            call dfactorial(((2*l)-1),dfact1)
            call dfactorial(((2*m)-1),dfact2)
            call dfactorial(((2*n)-1),dfact3)
            dmp=dfact1*dfact2*dfact3
-           !write(*,*) dmp 
            norm =((2.d0*alpha/pi)**0.75d0)*((np/dmp)**0.5d0)
-           !!write(*,*) norm,alpha
            x_a=x-xa 
            y_a=y-ya
            z_a=z-za
-           !write(*,*) x,y,z,r
 
           res=norm*(x_a**l)*(y_a**m)*(z_a**n)*&
           & exp(-alpha*x_a*x_a)*exp(-alpha*y_a*y_a)*exp(-alpha*z_a*z_a)
 
-      !!diff_x=l+m+n/r-(2.d0*alpha*r)
-      !!diff_y=((((l+m)*cos(theta))/sin(theta))-((n*sin(theta))/cos(theta))) !! FOR SP.GRID 
-      !!diff_z=(((m*cos(phi))/sin(phi))-((l*sin(phi))/cos(phi)))
-
-        diff_x=(l/x_a)-(2.0d0*alpha*x_a)
-        diff_y=(m/y_a)-(2.0d0*alpha*y_a) !! TEST FOR CCG GRID
-        diff_z=(n/z_a)-(2.0d0*alpha*z_a)
+          diff_x=(l/x_a)-(2.0d0*alpha*x_a)
+          diff_y=(m/y_a)-(2.0d0*alpha*y_a) 
+          diff_z=(n/z_a)-(2.0d0*alpha*z_a)
 
 
 
@@ -77,13 +68,6 @@
           grad_y=res*diff_y
           grad_z=res*diff_z
          
-         !! write(*,*)  grad_x,grad_y,grad_z
- 
-
-
-
-
-        !! write(*,*) res 
          end subroutine  position_wav
 
 
@@ -197,7 +181,6 @@
             call factorial(i,fact_i)
             call factorial((l-(2*i)),fact_l_i)
             temp1=temp1+(((-1)**i)*((px/sqrt(alpha))**(l-(2*i)))/((fact_i)*(fact_l_i)))
-             !!write(*,*)(px/sqrt(alpha))**(l-(2*i)) 
           end do
 
 
@@ -210,9 +193,7 @@
 
 
 
-            !! write(*,*) (px/sqrt(alpha))**(l-(2*i)-1)
           end do
-             !! write(*,*) diff_temp1,temp1
               
 
 
@@ -228,12 +209,8 @@
          do j=0,int(m/2)
              call factorial(j,fact_j)
              call factorial((m-(2*j)),diff_fact_m_j)
-             !diff_temp2=diff_temp2+(((-1)**j)*((py/sqrt(alpha))**(m-(2*j)-1))/((fact_j)*(diff_fact_m_j)))
              diff_temp2=diff_temp2+(((-1)**j)*&
              &(((1.d0/sqrt(alpha))**(m-(2*j)))*(m-(2*j))*(py**(m-(2*j)-1)))/((fact_j)*(diff_fact_m_j)))
-
-
-              !! write(*,*) diff_fact_m_j       
           end do
 
                
@@ -245,19 +222,14 @@
            call factorial((n-(2*k)),fact_n_k)
             temp3=temp3+(((-1)**k)*((pz/sqrt(alpha))**(n-(2*k)))/((fact_k)*(fact_n_k)))      
           end do
-         ! write(*,*) px,py,pz
-
 
           do k=0,int(n/2)
            call factorial(k,fact_k)
            call factorial((n-(2*k)),diff_fact_n_k)
-           !diff_temp3=diff_temp3+(((-1)**k)*((pz/sqrt(alpha))**(n-(2*k)-1))/((fact_k)*(diff_fact_n_k)))
-            
            diff_temp3=diff_temp3+(((-1)**k)*&
            &(((1.d0/sqrt(alpha))**(n-(2*k)))*(n-(2*k))*(pz**(n-(2*k)-1)))/((fact_k)*(diff_fact_n_k)))
       
           end do
-           !! write(*,*) diff_temp3
 
           
           res=temp*temp1*temp2*temp3
@@ -276,22 +248,6 @@
            res_diff_z=((-pz/(2*alpha))*res)+(t*za*res)+(temp*temp1*&
            &temp2*diff_temp3)
  
-
-
-  
-
-           !! write(*,*) real(res_diff_x),real(res_diff_y),real(res_diff_z),real(res)
-
-
-
-
-
-
-
-
-
-
-
 
 
 
